0124f37b109633898994e65bcf5e3144398a8b19
town <dash>
2026-08-31 15:47:55 -0400

    issue with link

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,4 +2,4 @@ export function cn(...classes: Array<string | false | null | undefined>) {
   return classes.filter(Boolean).join(" ");
 }
 
-export const meetingUrl = process.env.NEXT_PUBLIC_MEETING_URL || "https://teams.live.com/l/invite/FAAHS4JoQHO61ib7w?v=g1";
+export const meetingUrl = process.env.NEXT_PUBLIC_MEETING_URL || "https://teams.live.com/l/invite/FAAjMTaZgXBQEDDBw?v=g1";
